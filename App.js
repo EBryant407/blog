e97af05c5062566a80f174674281197bbbc7fe76
Eric Bryant <EBryant407@yahoo.com>
2021-05-10 18:10:00 -0400

    Wired up Context with BlogContext

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import IndexScreen from './src/Screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
@@ -14,4 +16,10 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <BlogProvider>
+    <App />
+  </BlogProvider>
+};
