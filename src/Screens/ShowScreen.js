3b938398906fb0f78cc4ad276531daa40582a79c
Eric Bryant <EBryant407@yahoo.com>
2021-06-05 20:42:51 -0400

    Created EditScreen

--- src/Screens/ShowScreen.js
+++ src/Screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context);
@@ -12,9 +13,22 @@ const ShowScreen = ({ navigation }) => {
         <View>
             <Text>
                 {blogPost.title}
-        </Text>
+            </Text>
+            <Text>
+                {blogPost.content}
+            </Text>
         </View>
     );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Edit', { id: navigation.getParam('id') })}>
+                <FontAwesome name="pencil" size={35} />
+            </TouchableOpacity>
+        ),
+    };
 };
 
 const styles = StyleSheet.create({});
