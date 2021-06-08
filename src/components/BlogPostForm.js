eefea25eddd9614fbe74e44da62f294ab2a5d124
Eric Bryant <EBryant407@yahoo.com>
2021-06-07 22:06:34 -0400

    Wiring up EditScreen, CreateScreen and BlostPostForm screen

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+    const [title, setTitle] = useState(initialValues.title);
+    const [content, setContent] = useState(initialValues.content);
     return (
         <View>
             <Text style={styles.label}>
@@ -16,6 +16,7 @@ const BlogPostForm = () => {
             <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
             <Button
                 title='Save Blog Post'
+                onPress={() => onSubmit(title, content)}
             />
         </View>
     );
