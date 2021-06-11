ceabbd9c3d062f205ce1dabd7eb620dfb1388058
Eric Bryant <EBryant407@yahoo.com>
2021-06-10 22:16:38 -0400

    Wired up EditScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,10 +2,17 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id
+                    ? action.payload
+                    : blogPost;
+            });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
-            return [...state, { id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content
+            return [...state, {
+                id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content
             }];
         default:
             return state;
@@ -23,6 +30,12 @@ const deleteBlogPost = dispatch => {
     return id => {
         dispatch({ type: 'delete_blogpost', payload: id })
     };
-}
+};
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]);
+const editBlogPost = dispatch => {
+    return (id, title, content) => {
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+    };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]);
